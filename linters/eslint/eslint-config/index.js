45d7cdf0c5d5393a0995fdca9dc5ba643e7a5670
Matthew LaForest <mattl@ted.com>
2019-07-23 15:26:55 -0400

    Fix error with eslint-comments rules

--- linters/eslint/eslint-config/index.js
+++ linters/eslint/eslint-config/index.js
@@ -1,16 +1,24 @@
+var error = 'error';
+var warn = 'warn';
+
 module.exports = {
   extends: [
     'eslint-config-airbnb',
     'eslint-config-prettier',
     'eslint-config-prettier/babel',
     'eslint-config-prettier/react',
-    'plugin:eslint-comments/recommended',
   ].map(require.resolve),
 
-  plugins: ['react-hooks'],
+  plugins: ['react-hooks', 'eslint-comments'],
 
   rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': error,
+    'react-hooks/exhaustive-deps': warn,
+    'eslint-comments/disable-enable-pair': error,
+    'eslint-comments/no-aggregating-enable': error,
+    'eslint-comments/no-duplicate-disable': error,
+    'eslint-comments/no-unlimited-disable': error,
+    'eslint-comments/no-unused-disable': warn,
+    'eslint-comments/no-unused-enable': warn,
   },
 };
